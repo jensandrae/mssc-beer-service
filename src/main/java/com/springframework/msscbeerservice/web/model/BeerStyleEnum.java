d5f6c924faeb3379184f251774c04d737efe2df4
Jens Andrae <andrae.dev@posteo.de>
2020-10-19 17:15:10 +0200

    Added web data model, updated pom.xml & .gitignore. Closes #2

---
+++ src/main/java/com/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,9 @@
+package com.springframework.msscbeerservice.web.model;
+
+/**
+ * Created by Jens on 2020-10-19
+ */
+public enum BeerStyleEnum {
+
+    LAGER, PILSENER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
