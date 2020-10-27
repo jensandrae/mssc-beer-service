65fdd351a9f6ee2962d28fd24e39dbb6e13a19c6
Jens Andrae <andrae.dev@posteo.de>
2020-10-27 14:07:38 +0100

    Added MapStruct. Closes #10

---
+++ src/main/java/com/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.springframework.msscbeerservice.web.mappers;
+
+import com.springframework.msscbeerservice.domain.Beer;
+import com.springframework.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by Jens Andrae on 2020-10-27
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto BeerToBeerDTO (Beer beer);
+
+    Beer BeerDTOToBeer (BeerDto beerDto);
+}
